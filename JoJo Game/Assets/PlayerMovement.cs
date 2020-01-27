9f30f36846ebb2abeb805ccae6fc4740df1dcc1d
Mark Busuttil <mark.busuttil.09@gmail.com>
2020-01-27 17:13:50 +0100

    Created Player Movement Script
    New player script :- player can move left and right, jump up and crouch down .

--- JoJo Game/Assets/PlayerMovement.cs
+++ JoJo Game/Assets/PlayerMovement.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour {
+
+    public CharaterController2D controller2D;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     
-    void Update()
-    {
-        
+
+    void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+
+        if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
     }
+
+    void FixedUpdate () 
+    {
+        controller2D.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
 }
