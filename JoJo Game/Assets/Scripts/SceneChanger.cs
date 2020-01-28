47b38353e9533895f297d4219f5d1b5c125ab21f
Mark Busuttil <mark.busuttil.09@gmail.com>
2020-01-28 14:58:53 +0100

    Camera Parallaxing
    Added parallaxing camera and fixed some minor issues with the player

---
+++ JoJo Game/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] public string _nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
